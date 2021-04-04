06195aa237606f6d41de07d56ffc19b29633bbe0
Zhen Guo <zhen.guo.2@slu.edu>
2021-04-04 18:36:59 +0800

    KWIC-2

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,9 +6,13 @@ function init(){
 }
 
 
-function findByTag(rowResult, tag){
-    selectedTag = tag.textContent;
-    $("#tagInput1").attr("value",selectedTag);
+function findByTag(selWord, tag, rowResult){
+  /*
+    selWord: selected word
+    rowResult: sentences
+    tag: POS
+  */
+    $("#tagInput1").attr("value",tag);
     var ulControl = $('#sentencesGroup');
     ulControl.find("li").remove();
     var rowResult1 = rowResult;
@@ -17,9 +21,14 @@ function findByTag(rowResult, tag){
         $('#clusterDiv1').show();
     }
     for(i=1; i<rowResult1.length+1; i++){
-        var ulcontent = "<li class=\"list-group-item d-flex justify-content-between align-items-center\">"
-                      + rowResult1[i-1] +
-                      "<span class=\"badge badge-primary badge-pill\">"+i+"</span>"+"</li>";
+       var wordIndex = rowResult1[i-1].toLowerCase().indexOf(selWord.toLowerCase());
+       var part1 = rowResult1[i-1].slice(0,wordIndex)
+       var part2 = rowResult1[i-1].slice(wordIndex, wordIndex + selWord.length+1)
+       var part3 = rowResult1[i-1].slice(wordIndex + selWord.length+1, rowResult1[i-1].length)
+       var ulcontent = "<li class=\"list-group-item d-flex justify-content-between align-items-center\">"
+                      + "<p>" + part1 + "<strong class=\"text-success\">" + part2 + "</strong>" + part3 + "</p>" +
+                      "<span class=\"badge badge-primary badge-pill\">"+i+"</span>"+
+                      "</li>";
         ulControl.append(ulcontent);
     }
 
