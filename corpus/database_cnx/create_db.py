3c1310d6d794990b4219f32ec5e3e11c761ab48b
W J Evans IV <70174172+hvkone@users.noreply.github.com>
2021-04-07 22:28:14 -0500

    Update create_db.py

--- corpus/database_cnx/create_db.py
+++ corpus/database_cnx/create_db.py
@@ -4,9 +4,10 @@ import mysql.connector as mariadb
 import mariadb
 import sys
 
-mariadb_cnx = mariadb.connect(user='zguo4@hopper.slu.edu', password='SJk+6L4K3fKX', database='psd_project', host='db1.mcs.slu.edu', port=3306)
 
-mycursor = mariadb_cnx.cursor(dictionary=True)
+# mariadb_cnx = mariadb.connect(user='zguo4@hopper.slu.edu', password='SJk+6L4K3fKX', database='psd_project', host='db1.mcs.slu.edu', port=3306)
+
+# mycursor = mariadb_cnx.cursor(dictionary=True)
 
 
 
